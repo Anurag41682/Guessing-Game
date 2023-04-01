dc3cec9ae50542b7a046131ddb34d0b15b13787c
Anurag41682 <anuragyadav41682@gmail.com>
2023-04-01 17:03:39 +0530

    initial-commit

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict';
+let toGuess = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+document.querySelector('.check').addEventListener('click', function () {
+  const guessed = Number(document.querySelector('.guess').value);
+  if (!guessed) {
+    document.querySelector('.message').textContent =
+      'Not valid Number Enter between 1 and 20 !';
+  } else if (guessed === toGuess) {
+    document.querySelector('.message').textContent = 'Correct Number !!';
+    document.querySelector('body').style.backgroundColor = '#196900';
+    document.querySelector('.number').style.width = '20rem';
+    document.querySelector('.number').style.color = '#be9200';
+    document.querySelector('.number').textContent = toGuess;
+    if (highScore < score) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else {
+    score--;
+    document.querySelector('.message').textContent =
+      guessed > toGuess ? 'Too High !!' : 'Too Low !!';
+  }
+  if (score <= 0) {
+    score = 0;
+    document.querySelector('.message').textContent = 'You Lost !! Game Over !!';
+  }
+  document.querySelector('.score').textContent = score;
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  toGuess = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.score').textContent = String(score);
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.color = '#333';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = 'hsl(192, 100%, 14%)';
+  document.querySelector('.number').style.width = '15rem';
+});
